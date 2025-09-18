e6de3eac6d2a4ee58e418702643f46d49a81f1a8
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-09-18 11:39:43 -0500

    feat: add nxt-sortablejs integration with sortable lists in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-
+import { SortablejsModule } from 'nxt-sortablejs';
+import { Options } from 'sortablejs';
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, SortablejsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Hello, FormCraftify');
+  cloneList1 = ['1', '2', '3', '4', '5'];
+
+  cloneList2 = ['6', '7', '8', '9', '10'];
+
+  clone1Options: Options = {
+    group: {
+      name: 'clone-group',
+      pull: 'clone',
+      put: false,
+    },
+  };
+
+  clone2Options: Options = {
+    group: 'clone-group',
+  };
 }
