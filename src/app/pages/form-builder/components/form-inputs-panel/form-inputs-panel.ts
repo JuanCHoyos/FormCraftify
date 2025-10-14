a7e231010bf1f50b3ea69eea1e527078a7af971e
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-10-13 23:15:18 -0500

    refactor: replace PrimeNG tabs with custom UI tabs component in form inputs panel

--- src/app/pages/form-builder/components/form-inputs-panel/form-inputs-panel.ts
+++ src/app/pages/form-builder/components/form-inputs-panel/form-inputs-panel.ts
@@ -1,19 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, model, signal } from '@angular/core';
-import { AsidePosition, UISearchInput, UISidebar } from '@shared/index';
-import { TabsModule } from 'primeng/tabs';
+import { FormsModule } from '@angular/forms';
+import { NavigationMode } from '@pages/form-builder/model/form-builder';
+import { UISearchInput, UISidebar } from '@shared/components/index';
+import { UITabsModule } from '@shared/components/ui-tabs/ui-tabs';
+import { AsidePosition, MenuItemType } from '@shared/types/ui.types';
 
 import { FormFieldList } from './components/form-field-list/form-field-list';
 
 @Component({
   selector: 'app-form-inputs-panel',
-  imports: [CommonModule, TabsModule, FormFieldList, UISearchInput, UISidebar],
+  imports: [CommonModule, FormsModule, FormFieldList, UISearchInput, UISidebar, UITabsModule],
   templateUrl: './form-inputs-panel.html',
 })
 export class FormInputsPanel {
   showSideBar = input<boolean>(false);
   isCollapsed = signal<boolean>(false);
-  search = model<string>('');
+  searchText = model<string>('');
 
+  items = signal<MenuItemType<number>[]>([
+    { id: 'tab-component', label: 'Components', value: 0, icon: 'lucideComponent' },
+    { id: 'tab-tree', label: 'Tree', value: 1, icon: 'lucideListTree' },
+  ]);
+  value = model(NavigationMode.SCROLL);
+  NavigationMode = NavigationMode;
   AsidePosition = AsidePosition;
 }
