86539574acbe17232784385371428bc4098e36ca
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-10-13 23:09:29 -0500

    feat:  :sparkles:  add UITabs component with tab navigation and content projection

---
+++ src/app/shared/components/ui-tabs/ui-tabs.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  computed,
+  contentChildren,
+  input,
+  model,
+  TemplateRef,
+  viewChild,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MenuItemType } from '@shared/types/ui.types';
+import { SelectButtonModule } from 'primeng/selectbutton';
+
+import { UIICon } from '../icon/ui-icon';
+
+@Component({
+  selector: 'ui-tab',
+  template: ` <ng-template #contentTemplate><ng-content></ng-content></ng-template> `,
+  standalone: true,
+})
+export class UITab {
+  value = input.required<MenuItemType['value']>();
+  template = viewChild<TemplateRef<unknown>>('contentTemplate');
+}
+
+@Component({
+  selector: 'ui-tabs',
+  imports: [CommonModule, FormsModule, SelectButtonModule, UIICon],
+  templateUrl: './ui-tabs.html',
+})
+export class UITabs {
+  tabs = contentChildren(UITab);
+  items = input.required<MenuItemType[]>();
+  value = model<MenuItemType['value']>(undefined);
+  currentActiveTab = computed<UITab | undefined>(() => {
+    const activeValue = this.value();
+    return this.tabs().find((tab) => tab.value() === activeValue);
+  });
+}
+
+export const UITabsModule = [UITabs, UITab];
