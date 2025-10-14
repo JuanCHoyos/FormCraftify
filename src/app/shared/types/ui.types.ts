d95e7378b4e8de894f04d2f8792202b1d2b59207
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-10-13 23:07:40 -0500

    feat: :sparkles: add UI type definitions for menu items, tree menus, and component variants

---
+++ src/app/shared/types/ui.types.ts
@@ -0,0 +1,35 @@
+import { LUCIDE_ICONS } from '@shared/components/icon/constants/icons';
+
+export interface MenuItemType<T = unknown> {
+  id: string;
+  label: string;
+  description?: string;
+  icon: LucideIconType;
+  value?: T;
+  handle?: () => void;
+}
+
+export interface TreeMenuType<T = unknown> {
+  id: string;
+  label: string;
+  items: MenuItemType<T>[];
+}
+
+export enum AsidePosition {
+  Left = 'left',
+  Right = 'right',
+}
+
+export enum UISearchVariant {
+  Filled = 'filled',
+  Outlined = 'outlined',
+}
+
+export enum HeadingType {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+}
+
+export type LucideIconType = keyof typeof LUCIDE_ICONS;
