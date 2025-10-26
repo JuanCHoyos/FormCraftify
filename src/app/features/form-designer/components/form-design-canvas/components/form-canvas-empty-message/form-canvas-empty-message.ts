74599d1d7c8be21267234f72ac6d4006c45f88e7
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-10-26 17:56:43 -0500

    feat: :sparkles: add drag and drop functionality to form canvas empty message

--- src/app/features/form-designer/components/form-design-canvas/components/form-canvas-empty-message/form-canvas-empty-message.ts
+++ src/app/features/form-designer/components/form-design-canvas/components/form-canvas-empty-message/form-canvas-empty-message.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { GroupFieldType } from '@core/formly/models/form-field-item';
 import { UIICon, UITitle } from '@shared/components/index';
+import { DropHover } from '@shared/directives/drop-hover';
 import { HeadingType } from '@shared/types/ui.types';
+import { SortablejsModule } from 'nxt-sortablejs';
+import { SortableOptions } from 'sortablejs';
 
 @Component({
   selector: 'app-form-canvas-empty-message',
-  imports: [UIICon, UITitle],
+  imports: [CommonModule, DropHover, UIICon, UITitle, SortablejsModule],
   templateUrl: './form-canvas-empty-message.html',
 })
 export class FormCanvasEmptyMessage {
+  groupField = input.required<GroupFieldType>();
+  sortableConfig = input.required<SortableOptions>();
   HeadingType = HeadingType;
 }
