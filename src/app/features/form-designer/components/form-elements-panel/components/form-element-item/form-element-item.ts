123ccd19ce30fe7271cf4fd6f97a35476fddaabe
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-10-27 23:59:59 -0500

    feat: :sparkles: improve drag preview styling for form element items

--- src/app/features/form-designer/components/form-elements-panel/components/form-element-item/form-element-item.ts
+++ src/app/features/form-designer/components/form-elements-panel/components/form-element-item/form-element-item.ts
@@ -9,6 +9,17 @@ import { MenuItemType } from '@shared/types/ui.types';
   selector: 'form-element-item',
   imports: [CommonModule, CdkDrag, CdkDragPlaceholder, UIICon],
   templateUrl: './form-element-item.html',
+  styles: `
+    .cdk-drag-preview {
+      padding: 4px 8px;
+      background: var(--p-primary-100);
+      border: 1px solid var(--p-primary-500) !important;
+      box-shadow:
+        0 5px 5px -3px rgba(0, 0, 0, 0.2),
+        0 8px 10px 1px rgba(0, 0, 0, 0.14),
+        0 3px 14px 2px rgba(0, 0, 0, 0.12);
+    }
+  `,
 })
 export class FormElementItem {
   item = input.required<MenuItemType<FormType>>();
